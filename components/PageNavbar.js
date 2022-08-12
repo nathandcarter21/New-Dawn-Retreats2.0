1b37a5298e00a8920f2360d4993c65dd85910653
Nathan Carter <97918203+nathandcarter21@users.noreply.github.com>
2022-08-12 14:56:36 -0700

    favicon

--- components/PageNavbar.js
+++ components/PageNavbar.js
@@ -7,6 +7,7 @@ const PageNavbar = () => {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
+						<Nav.Link href="/">Home</Nav.Link>
 						<Nav.Link href="/retreats">Retreats</Nav.Link>
 						<Nav.Link href="/about">About</Nav.Link>
 						<Nav.Link href="/contact">Contact</Nav.Link>
