620f461d7b8f245103d1fa2eb7f5ca520f38e599
Nathan Carter <97918203+nathandcarter21@users.noreply.github.com>
2022-08-17 12:29:33 -0700

    spam

--- pages/api/email.js
+++ pages/api/email.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 		const { name, email, message } = req.body;
 		if (name && email && message) {
 			console.log(name);
-			if (name === "CryptoCal") {
+			if (name === "CrytoCal") {
 				res.redirect(301, "/contact?success=true");
 				return;
 			}
