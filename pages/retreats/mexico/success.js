d5e9615707764f71190096e66d429e3f8240133d
Nathan Carter <97918203+nathandcarter21@users.noreply.github.com>
2023-02-08 17:36:38 -0700

    Added success page

---
+++ pages/retreats/mexico/success.js
@@ -0,0 +1,11 @@
+export default function Summer_Lake() {
+    return (
+        <main className="container-fluid pr-0 mt-4">
+            <div className="row text-center">
+                <div className="contourBtm mt-3">
+                    <h1 className="mb-5">Success</h1>
+                </div>
+            </div>
+        </main>
+    );
+}
