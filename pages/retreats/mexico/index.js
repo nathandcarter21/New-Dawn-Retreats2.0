448c412113fde0829d3dda1c195fec0a38f01a7b
Nathan Carter <nathandcarter21@gmail.com>
2024-12-14 12:48:03 -0700

    done

--- pages/retreats/mexico/index.js
+++ pages/retreats/mexico/index.js
@@ -9,24 +9,22 @@ export default function Summer_Lake() {
           </h1>
         </div>
       </div>
-      <div className="row text-center align-items-center lowBright contourBtm">
-        <div className="row text-center align-items-center lowBright">
-          <div className="col-lg-7 py-lg-5 pb-5 px-0">
-            <img src="/beachSunset2.jpg" alt="" className="img-fluid" />
-          </div>
-          <div className="col-lg-5 px-5 py-lg-5 pb-5 text-center">
-            <p className="my-0">
-              It is our sincere desire to invite grounded and mature women that
-              have some practice of self care already in place. Join us us on
-              this sacred rest experience to allow a deeper rest, and
-              restorative reset. Letting in new light, blessed by nature, and
-              connection with your highest self.
-            </p>
-          </div>
+      <div className="row text-center align-items-center contourBtm">
+        <div className="col-lg-7 py-lg-5 px-0 pb-5 order-1 order-lg-1">
+          <img src="/beachSunset2.jpg" alt="" className="img-fluid" />
+        </div>
+        <div className="col-lg-5 px-5 py-lg-5 pb-5 text-center order-2 order-lg-1">
+          <p className="my-0">
+            It is our sincere desire to invite grounded and mature women that
+            have some practice of self care already in place. Join us us on this
+            sacred rest experience to allow a deeper rest, and restorative
+            reset. Letting in new light, blessed by nature, and connection with
+            your highest self.
+          </p>
         </div>
       </div>
       <div className="row text-center align-items-center contourBtm">
-        <div className="col-lg-7 py-lg-5 px-0 pb-5 order-1 order-lg-2 ">
+        <div className="col-lg-7 py-lg-5 px-0 pb-5 order-2 order-lg-2 ">
           <p className="my-0 ">
             <ul style={{ fontSize: "18px", textAlign: "left" }}>
               <li>Sacred Dance and yoga practice </li>
@@ -49,7 +47,7 @@ export default function Summer_Lake() {
             </ul>
           </p>{" "}
         </div>
-        <div className="col-lg-5 px-5 py-lg-5 pb-5 text-centered order-2 order-lg-1">
+        <div className="col-lg-5 px-5 py-5 py-lg-5 pb-5 text-centered order-1 order-lg-1">
           <p className="my-0">
             Retreat includes 4 days and 5 nights at Casa Princesa Del Mar One
             two hour on-site massage and bodywork session. Two meals daily;
@@ -78,11 +76,64 @@ export default function Summer_Lake() {
             deposit requires{" "}
           </p>
           <p></p>
-          <p className="my-0">
+          {/* <p className="my-0">
             Airfare not included Total retreat cost $1500.00
-          </p>
+          </p> */}
           <p></p>
           <p className="my-0">Paid in full by 1/30/2025</p>
+        </div>
+      </div>
+      <div className="row text-center align-items-center lowBright my-0">
+        <div className="col-10 offset-1 py-5 text-centered">
+          <h2 className="my-0">Prices</h2>
+        </div>
+      </div>
+      <div className="row text-center lowBright pb-5 d-flex justify-content-evenly gap-3">
+        <div className="col-8 col-lg-2 offset-lg-0 rate rate1">
+          <div className="">
+            <p className="mt-3">Twin Bed</p>
+            <p>$1,300 w/o airfair</p>
+          </div>
+          <a
+            href="https://buy.stripe.com/8wMbK54NKcFG64wdQU"
+            className="btn btn-lg pinkBtn mb-3 rateBtn"
+          >
+            Sign Up
+          </a>
+        </div>
+        <div className="col-8 col-lg-2 offset-lg-0 rate rate2">
+          <div className="">
+            <p className="mt-3">Queen Bed</p>
+            <p>$1,400 w/o airfair</p>
+          </div>
+          <a
+            href="https://buy.stripe.com/7sIg0l3JGdJK9gIaEL"
+            className="btn btn-lg pinkBtn mb-3 rateBtn"
+          >
+            Sign Up
+          </a>
+        </div>
+        <div className="col-8 col-lg-2 offset-lg-0 rate rate2">
+          <div className="">
+            <p className="mt-3">King Bed</p>
+            <p>$1,500 w/o airfair</p>
+          </div>
+          <a
+            href="https://buy.stripe.com/5kA9BXfso8pqfF6cMR"
+            className="btn btn-lg pinkBtn mb-3 rateBtn"
+          >
+            Sign Up
+          </a>
+        </div>
+        <div className="col-8 col-lg-2 offset-lg-0 rate rate4">
+          <p className="mt-3">Upgraded Spa Package</p>
+          <p>$200</p>
+          <a
+            href="https://buy.stripe.com/dR6eWh2FC356eB214a"
+            className="btn btn-lg pinkBtn mb-3 rateBtn"
+          >
+            Sign Up
+          </a>
         </div>
       </div>
       <div className="row text-center align-items-center bigBright contourBtm">
@@ -94,60 +145,20 @@ export default function Summer_Lake() {
           </p>
         </div>
       </div>
-      {/* <div className="row text-center align-items-center lowBright my-0">
-        <div className="col-10 offset-1 py-5 text-centered">
-          <h2 className="my-0">Prices</h2>
+      <div className="row text-center align-items-center contourBtm">
+        <div className="col-lg-7 py-lg-5 px-0 pb-5 order-1 order-lg-1">
+          <img src="/beachSunset2.jpg" alt="" className="img-fluid" />
         </div>
-      </div>
-      <div className="row text-center lowBright mb-5">
-        <div className="col-8 col-lg-3 offset-2 offset-lg-0 rate rate1">
-          <p className="mt-3">Single</p>
-          <p>$600 w/o airfair</p>
-          <a
-            href="https://buy.stripe.com/8wMbK54NKcFG64wdQU"
-            className="btn btn-lg pinkBtn mb-3"
-          >
-            Sign Up
+        <div className="col-lg-5 px-5 py-lg-5 pb-5 text-center order-2 order-lg-1">
+          <p className="my-0">Hosted by Lisa Carter LMT 5924</p>
+          <p>Retreat Facilitator</p>
+          <p></p>
+          <p></p>
+          <p className="my-0">Featuring Terra McDonald LMT 20082 </p>
+          <p> Co-Creator</p>
+          <a href="/about" className="btn pinkBtn btn-lg">
+            See About Info
           </a>
-        </div>
-        <div className="col-8 col-lg-3 offset-2 offset-lg-0 rate rate2">
-          <p className="mt-3">Double</p>
-          <p>$1100 w/o airfair</p>
-          <a
-            href="https://buy.stripe.com/5kA9BXfso8pqfF6cMR"
-            className="btn btn-lg pinkBtn mb-3"
-          >
-            Sign Up
-          </a>
-        </div>
-        <div className="col-8 col-lg-3 offset-2 offset-lg-0 rate rate3">
-          <p className="mt-3">Upgraded Spa Package</p>
-          <p>$200</p>
-          <p>Includes massages and salt scrubs</p>
-          <a
-            href="https://buy.stripe.com/dR6eWh2FC356eB214a"
-            className="btn btn-lg pinkBtn mb-3"
-          >
-            Sign Up
-          </a>
-        </div>
-      </div> */}
-      <div className="row text-center align-items-center lowBright contourBtm">
-        <div className="row text-center align-items-center lowBright">
-          <div className="col-lg-7 py-lg-5 pb-5 px-0">
-            <img src="/beachSunset2.jpg" alt="" className="img-fluid" />
-          </div>
-          <div className="col-lg-5 px-5 py-lg-5 pb-5 text-center">
-            <p className="my-0">Hosted by Lisa Carter LMT 5924</p>
-            <p>Retreat Facilitator</p>
-            <p></p>
-            <p></p>
-            <p className="my-0">Featuring Terra McDonald LMT 20082 </p>
-            <p> Co-Creator</p>
-            <a href="/retreats/mexico" className="btn pinkBtn btn-lg">
-              See About Info
-            </a>
-          </div>
         </div>
       </div>
     </main>
